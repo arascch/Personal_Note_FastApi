f27b790d0b9e9eca4b7f8ab7cbbf32edfac91913
arasch <arasch.ask@gmail.com>
2026-09-17 09:42:35 +0330

    created note class

--- models.py
+++ models.py
@@ -1,7 +1,15 @@
 from sqlmodel import SQLModel , Field
+from datetime import datetime
 
 class User(SQLModel , table = True):
     id : int |None = Field(default=None , primary_key=True)
     username: str
     hashed_password: str
     privilege : str = "user"
+
+class Note(SQLModel , table = True):
+    title : str
+    content: str
+    is_important : bool = False
+    created_at = Field(default_factory=datetime.now)
+    user_id : int= Field(foreign_key="User.id")
