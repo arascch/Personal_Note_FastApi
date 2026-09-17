01e8f5683090c66710b0559c2b71d0f2f7eae1b6
arasch <arasch.ask@gmail.com>
2026-09-17 10:27:30 +0330

    create register route

---
+++ main.py
@@ -0,0 +1,20 @@
+from sqlmodel import Session
+from fastapi.responses import RedirectResponse
+from models import User , Note
+from fastapi import FastAPI , responses , Form
+
+app = FastAPI()
+
+def make_hash(password:str):
+    return password + "scrambled!"
+
+
+@app.post("/register")
+def register(username: str=Form(...) , password: str=Form(...)):
+    scrambled = make_hash(password)
+    new_user = User(username = username , hashed_password = scrambled)
+    with Session(engine) as session:
+        session.add(new_user)
+        session.commit()
+
+    return RedirectResponse(url="/login",status_code=303)
